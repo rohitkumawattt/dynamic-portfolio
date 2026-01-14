7e865ae2862f9f4b3943ca96a42aa422281fe4ae
rohitkumawattt <rohitmaroth3@gmail.com>
2026-01-14 23:35:14 +0530

    bug fix

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -4,6 +4,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port:process.env.SMTP_PORT,
+    secure:false,
     auth:{
         user:process.env.SMTP_USER,
         pass:process.env.SMTP_PASS,
